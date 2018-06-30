f53577eb500d3d93ba6f6f68374e46cd45136bae
codingpanda23 <league@WTS22.attlocal.net>
2018-06-30 15:58:08 -0700

    League Invaders(IP)
    Started game today.
    Im behind

---
+++ src/Game/LeagueInvaders.java
@@ -0,0 +1,10 @@
+package Game;
+
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(500, 800);
+	}
+}
