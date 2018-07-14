532af8042003773e3d99b000acae96f06b760913
codingpanda23 <league@WTS22.attlocal.net>
2018-07-14 16:00:07 -0700

    League Invaders(IP)
    Still part 1

--- src/Game/LeagueInvaders.java
+++ src/Game/LeagueInvaders.java
@@ -1,10 +1,23 @@
 package Game;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
+	class Game {
+		JFrame frame;
+	}
+
 	public static void main(String[] args) {
+
+	}
+
+	void setup() {
 		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.add(frame);
+		frame.setVisible(true);
 		frame.setSize(500, 800);
+
 	}
 }
