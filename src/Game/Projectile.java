cd5ca3d9544961aca6c6fc93f2c2ed7067383471
League <league@WTS8.attlocal.net>
2018-09-15 15:56:47 -0700

    Step 7

--- src/Game/Projectile.java
+++ src/Game/Projectile.java
@@ -1,6 +1,27 @@
 package Game;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Projectile extends GameObject{
+	int speed;
+	
+	Projectile(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		
+		speed = 10;
+	}
+	public void update() {
+		y = y-speed;
+		if (y < 0) {
+			
+		}
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(x, y, width, height);
+	}
 	
 	
 }
