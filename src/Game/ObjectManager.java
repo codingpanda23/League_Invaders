1b64d7d6706458a254615cb5331d2946d127779b
codingpanda23 <ashleyarner2005@gmail.com>
2018-09-23 17:02:35 -0700

    home-work step 10
    iterate?

--- src/Game/ObjectManager.java
+++ src/Game/ObjectManager.java
@@ -1,20 +1,25 @@
 package Game;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class ObjectManager {
 	Rocketship ship;
-	Projectile[]proj = {};
+	ArrayList<Projectile> proj; 
 	
 	ObjectManager(Rocketship object){
 		ship = object;
-		
+		proj= new ArrayList<Projectile>();
 	}
 	public void update(){
 		ship.update();
+		
 	}
 	public void draw(Graphics g) {
 		ship.draw(g);
+	}
+	public void addProjectile(Projectile proj){
+		
 	}
 	
 }
